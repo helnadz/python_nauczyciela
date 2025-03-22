24a9ad27b0705b452629c57e3fe30b87fb7403c6
helnadz <helena.nadzieja@op.pl>
2025-03-22 13:47:09 +0100

    sortowanie bombelkowe wersja 2

---
+++ liczby_pierwsze/generowanie.py
@@ -0,0 +1,17 @@
+def generuj_liczby_pierwsze(n):
+
+    dane = [True]*(n+1)
+    dane[0] = dane[1] = False
+    for i in range (2, int(n**0.5)+1 ):
+         if dane[i] == True:
+            for j in range(i**2, n+1, i):
+                dane[j] = False
+    wynik = []
+    for i in range(n+1):
+         if dane[i] == True:
+            wynik += [i]
+    return wynik
+if __name__ == '__main__':
+    liczby_pierwsze = generuj_liczby_pierwsze(120)
+    print(liczby_pierwsze)
+
--- liczby_pierwsze/test_pierwszosci.py
+++ liczby_pierwsze/test_pierwszosci.py
@@ -1,18 +1,16 @@
 
-def czy_pierwsza(liczba : int):
-    liczba =25
+def czy_pierwsza(liczba :int):
+    liczba = 25
 
-
-    if liczba <2:
+    if liczba < 2:
         return False
     if liczba %2 == 0 or liczba % 3 ==0 :
         return False
 
-    for i in range(5, int(liczba**0.5) +1, 6):
+    for i in range(5, int(liczba**0.5) + 1, 6):
         if liczba % i == 0 or liczba % (i+2) == 0:
             return False
     return True
-
-
 if __name__ == '__main__':
+
     print(czy_pierwsza(50))
