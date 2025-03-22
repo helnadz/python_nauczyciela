2e6852ced9398e2e9b86788e02142ba6361941f6
helnadz <helena.nadzieja@op.pl>
2025-03-22 12:42:10 +0100

    sortowanie bombelkowe wersja 1

---
+++ liczby_pierwsze/test_pierwszosci.py
@@ -0,0 +1,18 @@
+
+def czy_pierwsza(liczba : int):
+    liczba =54
+
+
+    if liczba <2:
+        return False
+    if liczba %2 == 0 or liczba % 3 ==0 :
+        return False
+
+    for i in range(6, int(liczba**0.5) +1, 6):
+        if liczba % i-1 == 0 or liczba % i+1 == 0:
+            return False
+    return True
+
+
+if __name__ == '__main__':
+    print(czy_pierwsza(50))
