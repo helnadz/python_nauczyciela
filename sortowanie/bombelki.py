233b8ce9c3e267f9e4e95a253605a6fb8e00f12f
helnadz <helena.nadzieja@op.pl>
2025-03-22 19:16:20 +0100

    wersja popraw zad1

--- sortowanie/bombelki.py
+++ sortowanie/bombelki.py
@@ -1,7 +1,16 @@
-def sortuj_przez_bombelki(dane):
+def sortuj_przez_bombelki():
     dane = [1, 6, 5, 8, 2]
     n = len(dane)
+    porownania = 0
+    zmiany = 0
     for i in range(0, n-1):
+
         for j in range(0, n-1-i):
+            porownania +=1
             if dane[j] > dane[j+1]:
                 dane[j], dane[j+1] = dane[j+1], dane[j]
+                zmiany +=1
+        print(dane)
+    print(porownania, zmiany)
+if __name__ == '__main__':
+    sortuj_przez_bombelki()
