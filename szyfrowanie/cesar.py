0deb0774b6e05d9a0f5188e4cbe056d937455eb9
helnadz <helena.nadzieja@op.pl>
2025-04-06 10:57:27 +0200

    casar

---
+++ szyfrowanie/cesar.py
@@ -0,0 +1,30 @@
+#alfabet = ""
+#for i in range(ord("a"), ord("z")+1):
+ #   alfabet+=(chr(i))
+#print(alfabet)
+
+
+alfabet = [chr(i) for i in range(ord('a'), ord('z') +1)]
+print(alfabet)
+def szyfruj(napis,klucz):
+    szyfr = [alfabet[(i+klucz)%len(alfabet)] for i in range(len(alfabet))]
+    wynik =""
+    for znak in napis:
+        if znak.islower():
+            indeks=ord(znak)-ord("a")
+            zaszyfrowany_znak=szyfr[indeks]
+            wynik+=zaszyfrowany_znak
+        else:
+            wynik+=znak
+    return wynik
+
+    print(szyfr)
+def odszyfruj(napis,klucz):
+    pass
+if __name__=='__main__':
+    napis = "ala ma kota"
+    klucz = 3
+    zaszyfrowany_napis = szyfruj(napis,klucz)
+    print(zaszyfrowany_napis)
+    odszyfrowany_napis = odszyfruj(napis,klucz)
+    print(odszyfrowany_napis)
